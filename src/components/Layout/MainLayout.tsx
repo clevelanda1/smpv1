a83a11ebb2617ab8f4c0348d0a319cd2bdc264c9
clevelanda1 <clevelandaus1@gmail.com>
2025-05-16 03:30:17 -0500

    changed mobile padding of form from px-2 to px-1

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -26,7 +26,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({
         isStoryScreen={isStoryScreen}
         onShowSavedStories={onShowSavedStories}
       />
-      <main className="flex-1 w-full max-w-7xl mx-auto py-8 sm:py-12 relative mt-[73px] px-2 sm:px-4">
+      <main className="flex-1 w-full max-w-7xl mx-auto py-8 sm:py-12 relative mt-[73px] px-1 sm:px-4">
         {children}
       </main>
       <Footer />
